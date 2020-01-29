6d97d7cb3481e165992f3096f68c1ac52736950f
Jan Macario <jmacario@GMU.EDU>
2020-01-29 16:05:43 -0500

    Implements all custom WordPress dashboard content boxes via the add_meta_box function, rather than some using the wp_add_dashboard_widget function, as the add_meta_box function provides more flexibility.

--- functions.php
+++ functions.php
@@ -69,40 +69,22 @@ unset($customizer);
 // Custom dashboard content
 
   // Add custom dashboard content
-  add_action('wp_dashboard_setup', 'gmuj_custom_dashboard_content');
+  add_action('wp_dashboard_setup', 'gmuj_custom_dashboard_meta_boxes');
     
-  function gmuj_custom_dashboard_content() {
+  function gmuj_custom_dashboard_meta_boxes() {
     /* Adds custom WordPress dashboard content boxes */
-    /* Dashboard widgets appear in the left column; Dashboard meta boxes appear in the right column. */
 
     global $wp_meta_boxes;
     
-    /* Add WordPress dashboard 'theme support' widget */
-    wp_add_dashboard_widget('gmuj_custom_dashboard_widget_theme_support', 'Mason WordPress Theme Support', 'gmuj_custom_dashboard_widget_theme_support');
+    /* Add WordPress dashboard 'theme info' meta box */
+    add_meta_box("gmuj_custom_dashboard_meta_box_theme_info", "Mason WordPress Theme Info", "gmuj_custom_dashboard_meta_box_theme_info", "dashboard","side");
 
-    /* Add WordPress dashboard 'Mason resources' widget */
-    wp_add_dashboard_widget('gmuj_custom_dashboard_widget_mason_resources', 'Mason Resources', 'gmuj_custom_dashboard_widget_mason_resources');
+     /* Add WordPress dashboard 'theme support' meta box */
+    add_meta_box("gmuj_custom_dashboard_meta_box_theme_support", "Mason WordPress Theme Support", "gmuj_custom_dashboard_meta_box_theme_support", "dashboard","normal");
 
     /* Add WordPress dashboard 'Mason resources' meta box */
-    add_meta_box("gmuj_custom_dashboard_meta_box_theme_info", "Mason WordPress Theme Info", "gmuj_custom_dashboard_meta_box_theme_info", "dashboard","side");
-  }
+    add_meta_box("gmuj_custom_dashboard_meta_box_mason_resources", "Mason Resources", "gmuj_custom_dashboard_meta_box_mason_resources", "dashboard","normal");
 
-  /* WordPress dashboard widget content: theme support */
-  function gmuj_custom_dashboard_widget_theme_support() {
-    echo '<p>Welcome to the Mason WordPress Theme!</p>';
-    echo '<p>Need help?</p>';
-    echo '<p>For general inquiries, contact the Mason webmaster team at <a href="mailto:webmaster@gmu.edu">webmaster@gmu.edu</a>.</p>';
-    echo '<p>For more in-depth questions or requests, please <a href="https://gmu.teamdynamix.com/TDClient/33/Portal/Requests/TicketRequests/NewForm?ID=HyCYnjyvSqI_" target="_blank">submit a ticket to the webmaster team</a>.</p>';
-  }
-
-  /* WordPress dashboard widget content: Mason resources */
-  function gmuj_custom_dashboard_widget_mason_resources() {
-    echo '<ul>';
-    echo '<li><a href="https://brand.gmu.edu/" target="_blank">Mason Brand Profile</a></li>';
-    echo '<li><a href="https://brand.gmu.edu/visual-identity-and-style/color/" target="_blank">Mason Colors</a></li>';
-    echo '<li><a href="https://webinfo.gmu.edu/" target="_blank">Mason Web Standards</a></li>';
-    echo '<li><a href="https://webdev.gmu.edu/" target="_blank">Mason Web Development</a></li>';
-    echo '</ul>';
   }
 
   /* WordPress dashboard meta box content: theme info */
@@ -114,6 +96,26 @@ unset($customizer);
       echo "<tr><th style='text-align:left; vertical-align:top;'>Banner Image</th><td>Minimum Size: 1000px wide by 270px high<br />Images taller than this minimum size will be centered in desktop view. As the window width shrinks on smaller screens the top and bottom of the image will become visible.</td></tr>";
       echo "</table>";
   }
+
+  /* WordPress dashboard widget content: theme support */
+  function gmuj_custom_dashboard_meta_box_theme_support() {
+    echo '<p>Welcome to the Mason WordPress Theme!</p>';
+    echo '<p>Need help?</p>';
+    echo '<p>For general inquiries, contact the Mason webmaster team at <a href="mailto:webmaster@gmu.edu">webmaster@gmu.edu</a>.</p>';
+    echo '<p>For more in-depth questions or requests, please <a href="https://gmu.teamdynamix.com/TDClient/33/Portal/Requests/TicketRequests/NewForm?ID=HyCYnjyvSqI_" target="_blank">submit a ticket to the webmaster team</a>.</p>';
+  }
+
+  /* WordPress dashboard meta box content: Mason resources */
+  function gmuj_custom_dashboard_meta_box_mason_resources() {
+    echo '<ul>';
+    echo '<li><a href="https://brand.gmu.edu/" target="_blank">Mason Brand Profile</a></li>';
+    echo '<li><a href="https://brand.gmu.edu/visual-identity-and-style/color/" target="_blank">Mason Colors</a></li>';
+    echo '<li><a href="https://webinfo.gmu.edu/" target="_blank">Mason Web Standards</a></li>';
+    echo '<li><a href="https://webdev.gmu.edu/" target="_blank">Mason Web Development</a></li>';
+    echo '</ul>';
+  }
+
+
 
 // End custom dashboard content
 

2d1f5c76807f08dd4bc57f481ac44a9d9bec5f34
Jan Macario <jmacario@GMU.EDU>
2020-01-31 16:44:51 -0500

    Implements initial integration with custom post types plugin.

--- functions.php
+++ functions.php
@@ -152,6 +152,21 @@ function enqueue_color_scheme_styles(){
 
 }
 
+// Enqueue stylesheets related to custom post types and taxonomies
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_types_styles' );
+
+function enqueue_custom_types_styles(){
+  // People
+  wp_enqueue_style('gmuj_custom_people',get_template_directory_uri().'/assets/css/custom-people.css');
+  // Events
+  wp_enqueue_style('gmuj_custom_events',get_template_directory_uri().'/assets/css/custom-events.css');
+  // Articles
+  wp_enqueue_style('gmuj_custom_articles',get_template_directory_uri().'/assets/css/custom-articles.css');
+}
+
+
+
+
 add_action( 'admin_enqueue_scripts', 'enqueue_date_picker' );
 
 function enqueue_date_picker(){
---
+++ template-parts/content-single-person.php
@@ -0,0 +1,35 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    
+    <header class="entry-header">
+
+        <?php if (has_post_thumbnail()) { ?>
+            <div class="post-thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div>
+        <?php } //endif; ?>
+        <h2 class="entry-title">Person: <?php the_title(); ?></h2>
+ 
+    </header><!-- .entry-header -->
+
+    <div class="entry-content">
+        <?php the_content(); ?>
+        <div class="clearfix"></div>
+
+        <?php
+        /**
+         * This wp_link_pages option adapt to use bootstrap pagination style.
+         */
+        wp_link_pages(array(
+            'before' => '<div class="page-links">' . 'Pages:' . ' <ul class="pagination">',
+            'after'  => '</ul></div>',
+            'separator' => ''
+        ));
+        ?>
+    </div><!-- .entry-content -->
+
+    <footer class="entry-meta">
+ 
+
+    </footer><!-- .entry-meta -->
+
+</article>

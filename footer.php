5706156fdf6fca8172abaede89c475099ea88e9a
Jan Macario <jmacario@GMU.EDU>
2020-02-03 22:39:11 -0500

    Cleans-up theme template footer.

--- footer.php
+++ footer.php
@@ -1,10 +1,5 @@
-<?php
-/**
- * The theme footer.
- *
- * @package bootstrap-basic4
- */
-?>
+<!-- Site template footer -->
+
                     </div><!--.site-content-->
                 </div><!--.page-container-->
             </main>
@@ -23,54 +18,18 @@
                     </div>
                 </div>
 
-            </footer><!--.page-footer-->
+            </footer>
 
-
-            <!--wordpress footer-->
+            <!--WordPress footer-->
             <?php wp_footer(); ?>
-            <!--end wordpress footer-->
 
         </div><!-- #page -->
-        <script>
-        jQuery( document ).ready(function() {
-
-
-          var timeref = setInterval(function(){
-          jQuery(".caret-down").click(function () {
-          //alert("asdas");
-        });
-        });
-        }, 1000);
-
-        jQuery(function ($) {
-            $.ajaxSetup({ cache: false });
-            var $ed = $('.so-widget-sow-editor');
-            if($ed.length) {
-                $ed.each(function(){
-                    var $wt = $(this).find('h3.widget-title');
-                    var $ct = $(this).find('.textwidget h2');
-                    if($wt.length && $ct.length){
-                        $wt.hide();
-                        if($wt.html().length < 3){
-                            $ct.html($wt.html()+' '+$ct.html());
-                            $wt.remove();
-                        } else {
-                            $wt.show();
-                        }
-                    }
-                });
-            }
-
-        });
-
-        </script>
 
         <!--Custom javascript in theme footer.php file -->
         <script>
             jQuery(document).bind('keyup', function(e){
                 if(e.which==68) {
                     // "d"
-                    //jQuery('div#debug').css('display','block');
                     jQuery('div#debug').toggle();
                 }
             });                

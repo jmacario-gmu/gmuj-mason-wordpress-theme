f2559913495348b578eacb7d10b4f98f378bd1d5
Jan Macario <jmacario@GMU.EDU>
2020-02-04 10:43:55 -0500

    Cleans-up index.php file and adds a 'no posts' content template part.

--- index.php
+++ index.php
@@ -1,35 +1,39 @@
-<?php
-/**
- * The main template file.
- * 
- * To override home page (for listing latest post) add home.php into the theme.<br>
- * If front page displays is set to static, the index.php file will be used.<br>
- * If front-page.php exists, it will be override any home page file such as home.php, index.php.<br>
- * To learn more please go to https://developer.wordpress.org/themes/basics/template-hierarchy/ .
- * 
- * @package bootstrap-basic4
- */
+<!-- Main template -->
 
+<!--
+* To override home page (for listing latest post) add home.php into the theme.<br>
+* If front page displays is set to static, the index.php file will be used.<br>
+* If front-page.php exists, it will be override any home page file such as home.php, index.php.<br>
+* To learn more please go to https://developer.wordpress.org/themes/basics/template-hierarchy/ .
+-->
 
-// begins template. -------------------------------------------------------------------------
-get_header();
-?> 
-                <main id="main" class="col-md-<?php echo \BootstrapBasic4\Bootstrap4Utilities::getMainColumnSize(); ?> site-main" role="main">
-                    <?php
-                    if (have_posts()) {
-                        while (have_posts()) {
-                            the_post();
-                            get_template_part('template-parts/content', get_post_format());
-                        }// endwhile;
+<!-- Header -->
+<?php get_header(); ?> 
 
-                        $Bsb4Design = new \BootstrapBasic4\Bsb4Design();
-                        $Bsb4Design->pagination();
-                        unset($Bsb4Design);
-                    } else {
-                        get_template_part('template-parts/section', 'no-results');
-                    }// endif;
-                    ?> 
-                </main>
-<?php
-get_sidebar();
-get_footer();
+<!-- Main body -->
+<main id="main" class="col-md-<?php echo \BootstrapBasic4\Bootstrap4Utilities::getMainColumnSize(); ?> site-main" role="main">
+    <?php
+    // Do we have posts?
+        if (have_posts()) {
+            // Loop through posts
+                while (have_posts()) {
+                    // Display posts
+                    the_post();
+                    get_template_part('template-parts/content');
+                }
+            // Pagination
+                $Bsb4Design = new \BootstrapBasic4\Bsb4Design();
+                $Bsb4Design->pagination();
+                unset($Bsb4Design);
+        } else {
+            // Show no posts content
+                get_template_part('template-parts/content', 'no-posts');
+        }
+    ?> 
+</main>
+
+<!-- Sidebar -->
+<?php get_sidebar(); ?>
+
+<!-- Footer -->
+<?php get_footer();
---
+++ template-parts/content-no-posts.php
@@ -0,0 +1,12 @@
+<!-- 'no posts' section -->
+<section class="no-posts not-found">
+
+	<header class="intro">
+		<h1 class="entry-title">No Posts Found</h1>
+	</header>
+
+	<div class="page-content row-with-vspace">
+		<p>No posts found.</p>
+	</div>
+
+</section>
